58944b1e3850ede3d2e7c32435050bb1332aa127
CosmicSand <skyjlen@gmail.com>
2023-12-17 23:24:51 +0200

    Update 02-form.js

---
+++ src/js/02-form.js
@@ -0,0 +1,26 @@
+'use stict';
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', event => {
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+
+  const data = {
+    email,
+    message,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
+});
+
+if (localStorage.getItem('feedback-form-state')) {
+  const dataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+  form.elements.email.value = dataFromLS.email;
+  form.elements.message.value = dataFromLS.message;
+}
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const storedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  console.log(storedData);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+});
