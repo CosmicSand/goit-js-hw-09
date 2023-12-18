0da082bc539995ff4746418b05f64287b3b17fe3
CosmicSand <skyjlen@gmail.com>
2023-12-18 23:45:25 +0200

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,8 @@
 'use stict';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const images = [
   {
@@ -89,18 +93,13 @@ function galleryCreation() {
 
 galleryCreation();
 
-// Описаний в документації
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
   fadeSpeed: 150,
   className: 'lightbox-on',
   closeText:
-    '<svg class="icon" width="32" height="32" aria-label="close icon"><use href="./img/icons.svg#close"></use>/svg>',
+    '<svg class="icon" width="32" height="32" aria-label="close icon"><use href="../../src/img/icons.svg#close"></use>/svg>',
   navText: [
     '<svg class="icon" width="24" height="24" aria-label="to the left icon"><use href="./img/icons.svg#chevron-left"></use>/svg>',
     '<svg class="icon" width="24" height="24" aria-label="to the rigth icon"><use href="./img/icons.svg#chevron-right"></use>/svg>',
